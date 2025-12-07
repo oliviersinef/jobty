7ab60275123657bb23600d0d5d6995a0ccf1d09b
olivier sinefeu <oliviersinefeu@gmail.com>
2025-12-08 00:40:14 +0100

    Update: nouveau menu navigation découverte

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Decouverte from './pages/Decouverte'; // ✅ Majuscule
+import Decouverte from './pages/decouverte'; // ✅ Majuscule
 import Connexion from './pages/connexion';
 import Marketplace from './pages/marketplace';
 import Portfolio from './pages/portfolio'; // ✅ Vérifiez aussi celui-ci
